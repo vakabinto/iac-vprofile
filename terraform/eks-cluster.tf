99c24712e8b5c1d1022a7a276caf000b3da5d547
vakabinto <vakabinto@gmail.com>
2024-04-12 14:46:44 +0100

    action

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 20.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.29"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "~> 5.0.0"
 
   name = "vprofile-eks"
 
